c6612b5dc6922af361a34c0bf6bb54f0dce38c18
Robert <packleader@dumbdogentertainment.com>
2020-06-07 00:36:27 -0500

    -- Earth's seconds as a constant;

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -2,37 +2,39 @@ package space
 
 type Planet string
 
+const EarthSecondsPerYear = 31557600
+
 func Age(seconds float64, planet Planet) float64 {
 	if planet == "Mercury" {
-		return seconds / 0.2408467 / 31557600
+		return seconds / 0.2408467 / EarthSecondsPerYear
 	}
 
 	if planet == "Venus" {
-		return seconds / 0.61519726 / 31557600
+		return seconds / 0.61519726 / EarthSecondsPerYear
 	}
 
 	if planet == "Earth" {
-		return seconds / 1 / 31557600
+		return seconds / 1 / EarthSecondsPerYear
 	}
 
 	if planet == "Mars" {
-		return seconds / 1.8808158 / 31557600
+		return seconds / 1.8808158 / EarthSecondsPerYear
 	}
 
 	if planet == "Jupiter" {
-		return seconds / 11.862615 / 31557600
+		return seconds / 11.862615 / EarthSecondsPerYear
 	}
 
 	if planet == "Saturn" {
-		return seconds / 29.447498 / 31557600
+		return seconds / 29.447498 / EarthSecondsPerYear
 	}
 
 	if planet == "Uranus" {
-		return seconds / 84.016846 / 31557600
+		return seconds / 84.016846 / EarthSecondsPerYear
 	}
 
 	if planet == "Neptune" {
-		return seconds / 164.79132 / 31557600
+		return seconds / 164.79132 / EarthSecondsPerYear
 	}
 
 	return 0
