c506dc92ab91bcf354849c59f6fc4f705be31deb
Robert <packleader@dumbdogentertainment.com>
2020-06-27 12:05:33 -0500

    -- updating formatting;

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -9,9 +9,9 @@ import "fmt"
 
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-    if name == "" {
-        return "One for you, one for me."
-    }
+	if name == "" {
+		return "One for you, one for me."
+	}
 
 	return fmt.Sprintf("One for %s, one for me.", name)
 }
